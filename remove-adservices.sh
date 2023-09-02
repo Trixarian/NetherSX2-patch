74dd7362084824a18c3417b64bba8332a3b021a1
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-09-02 09:35:34 +0200

    Update remove-adservices.sh

--- remove-adservices.sh
+++ remove-adservices.sh
@@ -13,7 +13,7 @@ if command -v "aapt" >/dev/null 2>&1; then
 	aapt r 15210-v1.5-4248-noads.apk play-services-ads-identifier.properties
 	aapt r 15210-v1.5-4248-noads.apk play-services-ads-base.properties
 else
-	chmod +x lib/aaptlinux lib/apksignerlinux
+	chmod +x lib/aaptlinux
 	lib/aaptlinux r 15210-v1.5-4248-noads.apk user-messaging-platform.properties
 	lib/aaptlinux r 15210-v1.5-4248-noads.apk play-services-tasks.properties
 	lib/aaptlinux r 15210-v1.5-4248-noads.apk play-services-measurement-sdk-api.properties
@@ -31,11 +31,11 @@ fi
 if command -v "apksigner" >/dev/null 2>&1; then
 	apksigner sign --ks lib/android.jks --ks-pass pass:android 15210-v1.5-4248-noads.apk
 else
-	lib/apksignerlinux sign --ks lib/android.jks --ks-pass pass:android 15210-v1.5-4248-noads.apk
+	exec java -jar lib/lib/apksigner.jar sign --ks lib/android.jks --ks-pass pass:android 15210-v1.5-4248-noads.apk
 fi
 # Alternate Key:
 # if command -v "apksigner" >/dev/null 2>&1; then
 # 	apksigner sign --ks lib/public.jks --ks-pass pass:public 15210-v1.5-4248-noads.apk
 # else
-# 	lib/apksignerlinux sign --ks lib/public.jks --ks-pass pass:public 15210-v1.5-4248-noads.apk
+# 	exec java -jar lib/lib/apksigner.jar sign --ks lib/public.jks --ks-pass pass:public 15210-v1.5-4248-noads.apk
 # fi
