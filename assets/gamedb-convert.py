2819613c973248f46eabe40eb74a6a70d5a1f293
Trixarian <admin@trixarian.net>
2024-03-20 13:46:41 +0200

    Update gamedb-convert.py

--- assets/gamedb-convert.py
+++ assets/gamedb-convert.py
@@ -2,22 +2,17 @@
 # A simple script to make comparing GameDB files easier for NetherSX2
 # Just drag and drop the GameIndex.yaml file from PCSX2 on to it to process it
 #
-
 import re, sys
 
-# Checks if a file is specified or has been dropped on to the script
 if len(sys.argv) > 1: gamedb_file = sys.argv[1]
 else: 
     print('Usage: gamedb-convert.py GameIndex.yaml')
     sys.exit()
 
-# List of settings we want to exclude from the GameDB:
-ignore_list = ['bilinearUpscale', 'cpuSpriteRenderLevel', 'eeCycleRate', 'eeDivRoundMode', 'GSC_PolyphonyDigitalGames', 'GSC_HitmanBloodMoney', 'GSC_NFSUndercover', 'GSC_MetalGearSolid3', 'name-sort', 'nativePaletteDraw', 'OI_HauntingGround']
-# List of what we want to replace in the GameDB:
-replace_dic = {'autoFlush: 2': 'autoFlush: 1', 'instantVU1:': 'InstantVU1SpeedHack:', 'halfPixelOffset: 4': 'halfPixelOffset: 1', 'mtvu:': 'MTVUSpeedHack:', 'mvuFlag:': 'mvuFlagSpeedHack:', 'minimumBlendingLevel:': 'recommendedBlendingLevel:', 'name-en:': 'name:'}
+ignore_list = ['bilinearUpscale', 'cpuSpriteRenderLevel', 'eeCycleRate', 'eeDivRoundMode', 'GSC_HitmanBloodMoney', 'GSC_MetalGearSolid3', 'GSC_NFSUndercover', 'GSC_PolyphonyDigitalGames', 'name-sort', 'nativePaletteDraw', 'OI_HauntingGround']
+replace_dic = {'autoFlush: 2': 'autoFlush: 1', 'halfPixelOffset: 4': 'halfPixelOffset: 1', 'instantVU1:': 'InstantVU1SpeedHack:', 'minimumBlendingLevel:': 'recommendedBlendingLevel:', 'mtvu:': 'MTVUSpeedHack:', 'mvuFlag:': 'mvuFlagSpeedHack:', 'name-en:': 'name:'}
 
 with open(gamedb_file, encoding='utf8') as oldfile, open('GameIndex[fixed].yaml', 'w', encoding='utf8') as newfile:
-    # Let's add a single line backbuffer to check later
     prev_line = ''
     for line in oldfile:
         for key in replace_dic:
