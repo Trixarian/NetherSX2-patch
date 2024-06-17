35e73347b2427ceb671d4beacea7ef451724e544
Trixarian <admin@trixarian.net>
2024-06-17 18:32:14 +0200

    Updated GameDB convertor

--- assets/gamedb-convert.py
+++ assets/gamedb-convert.py
@@ -1,9 +1,8 @@
-#
 # A simple script to make comparing GameDB files easier for NetherSX2
 # Just drag and drop the GameIndex.yaml file from PCSX2 on to it to process it
-# Requires the ruamel.yaml library work:
-# pip install ruamel.yaml from your terminal
-#
+# Requires the ruamel.yaml library to be installed to work
+# Type pip install ruamel.yaml in the terminal to install it
+
 import re, sys, os
 from ruamel.yaml import YAML
 
@@ -25,11 +24,13 @@ yaml.allow_duplicate_keys = True
 yaml.width = 512
 yaml.representer.add_representer(type(None), my_represent_none)
 
-# Create our filters to help process the file
+# Create filters to process the file with
 char_list = ['ß', 'à', 'á', 'â', 'ã', 'ä', 'å', 'æ', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ð', 'ñ', 'ò', 'ó', 'ô', 'õ', 'ö', 'ø', 'ù', 'ú', 'û', 'ü', 'ý', 'þ', 'ÿ', '¸', 'À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Æ', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ð', 'Ñ', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'Ø', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'Þ']
 ignore_list = ['bilinearUpscale', 'cpuSpriteRenderLevel', 'eeCycleRate', 'eeDivRoundMode', 'GSC_HitmanBloodMoney', 'GSC_MetalGearSolid3', 'GSC_NFSUndercover', 'GSC_PolyphonyDigitalGames', 'name-sort', 'nativePaletteDraw', 'OI_HauntingGround']
 replace_dic = {'autoFlush: 2': 'autoFlush: 1', 'forceEvenSpritePosition:': 'wildArmsHack:', 'halfPixelOffset: 4': 'halfPixelOffset: 1', 'instantVU1:': 'InstantVU1SpeedHack:', 'minimumBlendingLevel:': 'recommendedBlendingLevel:', 'mtvu:': 'MTVUSpeedHack:', 'mvuFlag:': 'mvuFlagSpeedHack:', 'name-en:': 'name:'}
 
+# Process file to make it compatible with NetherSX2
+print('Processing the GameDB file...')
 with open(gamedb_file, encoding='utf8') as oldfile, open('GameIndex[fixed].yaml', 'w', encoding='utf8') as newfile:
     prev_line = ''
     for line in oldfile:
@@ -45,6 +46,7 @@ with open(gamedb_file, encoding='utf8') as oldfile, open('GameIndex[fixed].yaml'
         prev_line = line
 
 # Uses the Magic of YAML Processing to clean up empty keys
+print('Removing the empty keys from the GameDB...')
 with open('GameIndex[fixed].yaml', encoding='utf8') as newfile, open('GameIndex[temp].yaml', 'w', encoding='utf8') as tempfile:
     data = yaml.load(newfile)
     yaml.dump(data, tempfile)
@@ -53,6 +55,5 @@ with open('GameIndex[temp].yaml', encoding='utf8') as tempfile, open('GameIndex[
         if '{' in line: line = line.replace('{', '{ ')
         if '}' in line: line = line.replace('}', ' }')
         if ': null' not in line: newfile.write(line)
-
-# Remove the temp file
+print('All Done!')
 os.remove('GameIndex[temp].yaml')
