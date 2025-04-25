a82ba32e0110bbde03c97007deea9ed507c4c940
Trixarian <admin@trixarian.net>
2025-04-25 09:05:55 +0200

    Update to Latest from Saramagrean

--- assets/gamedb-convert.py
+++ assets/gamedb-convert.py
@@ -2,7 +2,6 @@
 # Just drag and drop the GameIndex.yaml file from PCSX2 on to it to process it
 # Requires the ruamel.yaml library to be installed to work
 # Type pip install ruamel.yaml in the terminal to install it
-
 import re, sys, os
 from ruamel.yaml import YAML
 
@@ -15,8 +14,33 @@ else:
 def my_represent_none(self, data):
     return self.represent_scalar(u'tag:yaml.org,2002:null', u'null')
 
+# Process file to make it compatible with NetherSX2
+def process_db(file_name, clean_name):
+    print('Processing ' + file_name + '...')
+    if os.path.isfile(clean_name) and clean_name == 'GameIndex[fixed].yaml': os.remove('GameIndex[fixed].yaml')
+    if os.path.isfile('GameIndex[temp].yaml'): os.remove('GameIndex[temp].yaml')
+    with open(file_name, encoding='utf8') as newfile, open('GameIndex[temp].yaml', 'w', encoding='utf8') as tempfile:
+        prev_line = ''
+        for line in newfile:
+            for key in replace_dic:
+                if key in line: line = line.replace(key, replace_dic[key])
+            if re.search(r'moveHandler: \".+\"', line): line = re.sub(r'moveHandler: \".+\"', 'textureInsideRT: 1', line)
+            # We need the following four checks to fix ONE issue caused by someone inconsistently adding moonrunes to the GameDB
+            if line == prev_line: continue
+            if 'name:' in line and '"' not in line: continue
+            if 'name:' in line and re.search(r'[A-Z]+: ', line): continue
+            if not any(safe_char in line for safe_char in char_list) and not line.isascii(): continue
+            if not any(ignore_word in line for ignore_word in ignore_list): tempfile.write(line)
+            prev_line = line
+    os.rename('GameIndex[temp].yaml', clean_name)
+
 # Fixes issues created by yaml processing and removes empty keys
 def fix_db(file_name):
+    # Uses the Dark Arts (YAML Processing) to work it's magic
+    print('Removing empty keys from ' + file_name + '...')
+    with open(file_name, encoding='utf8') as newfile, open('GameIndex[temp].yaml', 'w', encoding='utf8') as tempfile:
+        data = yaml.load(newfile)
+        yaml.dump(data, tempfile)
     with open('GameIndex[temp].yaml', encoding='utf8') as tempfile, open(file_name, 'w', encoding='utf8') as newfile:
         for line in tempfile:
             if '{' in line: line = line.replace('{', '{ ')
@@ -38,27 +62,9 @@ char_list = ['ß', 'à', 'á', 'â', 'ã', 'ä', 'å', 'æ', 'ç', 'è', 'é', '
 ignore_list = ['bilinearUpscale', 'cpuSpriteRenderLevel', 'eeCycleRate', 'eeDivRoundMode', 'GSC_HitmanBloodMoney', 'GSC_MetalGearSolid3', 'GSC_NFSUndercover', 'GSC_PolyphonyDigitalGames', 'name-sort', 'nativePaletteDraw', 'nativeScaling', 'OI_HauntingGround']
 replace_dic = {'autoFlush: 2': 'autoFlush: 1', 'forceEvenSpritePosition:': 'wildArmsHack:', 'halfPixelOffset: 4': 'halfPixelOffset: 1', 'instantVU1:': 'InstantVU1SpeedHack:', 'mtvu:': 'MTVUSpeedHack:', 'mvuFlag:': 'mvuFlagSpeedHack:', 'name-en:': 'name:'}
 
-# Process file to make it compatible with NetherSX2
-print('Processing the GameDB file...')
-with open(gamedb_file, encoding='utf8') as oldfile, open('GameIndex[fixed].yaml', 'w', encoding='utf8') as newfile:
-    prev_line = ''
-    for line in oldfile:
-        for key in replace_dic:
-            if key in line: line = line.replace(key, replace_dic[key])
-        if re.search(r'moveHandler: \".+\"', line): line = re.sub(r'moveHandler: \".+\"', 'textureInsideRT: 1', line)
-        # We need the following four checks to fix ONE issue caused by a moron that inconsistently added moonrunes to the GameDB
-        if line == prev_line: continue
-        if 'name:' in line and '"' not in line: continue
-        if 'name:' in line and re.search(r'[A-Z]+: ', line): continue
-        if not any(safe_char in line for safe_char in char_list) and not line.isascii(): continue
-        if not any(ignore_word in line for ignore_word in ignore_list): newfile.write(line)
-        prev_line = line
-
-# Uses the Dark Arts (YAML Processing) to work it's magic
-print('Removing the empty keys from the GameDB...')
-with open('GameIndex[fixed].yaml', encoding='utf8') as newfile, open('GameIndex[temp].yaml', 'w', encoding='utf8') as tempfile:
-    data = yaml.load(newfile)
-    yaml.dump(data, tempfile)
+# This is where the Magic Happens
+process_db(gamedb_file, 'GameIndex[fixed].yaml')
+print('Creating GameIndex[fixed].yaml...')
 fix_db('GameIndex[fixed].yaml')
 with open('GameIndex[fixed].yaml', encoding='utf8') as base, open('old/GameIndex[4248].yaml', encoding='utf8') as og, open('GameIndex[diff].yaml', encoding='utf8') as diff, open('GameIndex[temp].yaml', 'w', encoding='utf8') as merged:
     print('Loading GameDB entries to merge...')
@@ -70,5 +76,9 @@ with open('GameIndex[fixed].yaml', encoding='utf8') as base, open('old/GameIndex
     base_db.update(diff_db)
     print('Creating GameIndex[merged].yaml file...')
     yaml.dump(base_db, merged)
+# Have to make sure there isn't any bad entries in the merged file
+process_db('GameIndex[merged].yaml', 'GameIndex[temp2].yaml')
+os.remove('GameIndex[merged].yaml')
+os.rename('GameIndex[temp2].yaml', 'GameIndex[merged].yaml')
 fix_db('GameIndex[merged].yaml')
 print('All Done!')
